da81a50cb173a3f1a9b2a2b84eb9f963c05b6609
Constantin-Alin <clmalex99@gmail.com>
2024-12-26 13:27:36 +0100

    Added thrusters message

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,6 +1,10 @@
 public class Rocket {
     public static void main(String[] args) {
         for (int i = 10; i >= 0; i--) {
+            if (i == 5) {
+                System.out.println("Thrusters starting!");
+                continue;
+            }
             System.out.println(i);
         }
     }
