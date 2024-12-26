f8cc6c779b33f337b6662135f2367c07edecd1e9
Constantin-Alin <clmalex99@gmail.com>
2024-12-26 13:36:33 +0100

    Add: thread.sleep

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,5 +1,5 @@
 public class Rocket {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 10; i >= 0; i--) {
             if (i == 5) {
                 System.out.println("Thrusters starting!");
@@ -10,6 +10,7 @@ public class Rocket {
                 continue;
             }
             System.out.println(i);
+            Thread.sleep(1000);
         }
     }
 }
